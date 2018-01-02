1502b55e5112e4fb19a000c4310867060b80b506
Tem Abraha <temabraha1@gmail.com>
2018-01-01 18:05:03 -0600

    Cleaned up index and fragments, added "add.html", with a handler GET request.

--- src/main/java/com/payleaf/support/controllers/UserController.java
+++ src/main/java/com/payleaf/support/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.payleaf.support.controllers;
 
+import com.payleaf.support.models.User;
+import com.payleaf.support.models.data.IssueDao;
 import com.payleaf.support.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("support")
@@ -12,6 +15,9 @@ public class UserController {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private IssueDao issueDao;
 
     // Request path: /support
     @RequestMapping(value = "")
@@ -29,6 +35,14 @@ public class UserController {
         model.addAttribute("title", "About Payleaf");
 
         return "about/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddUserForm(Model model) {
+
+        model.addAttribute("title", "Add a user");
+        model.addAttribute(new User());
+        return "user/add";
     }
 
 }
